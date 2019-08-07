014b9e2f49e6ab4ad13d1f700f2fe636675f0441
manxisuo <manxisuo@gmail.com>
2019-08-07 21:46:31 +0800

    update

--- groot.py
+++ groot.py
@@ -2,13 +2,66 @@ import requests
 from bs4 import BeautifulSoup
 from queue import Queue
 import urllib.request
+import re
 import os
 import os.path
-import re
+import threading
 
 
-Q = Queue() # URL队列
+q = Queue() # URL队列
 RULE_DICT = {} # 用户注册的各级页面处理逻辑
+
+# 页面处理任务
+class PageTask:
+    def __init__(self, level, url):
+        self.level = level
+        self.url = url
+
+    def run(self):
+        html = requests.get(self.url).text
+        rules = RULE_DICT[self.level]()
+
+        for extractor, action in rules.items():
+
+            if isinstance(extractor, Re): # 正则抽取模式
+                vals = extractor.extract(None, html)
+                for i, val in enumerate(vals):
+                    if callable(action): # 自定义函数
+                        action(val)
+                    else:
+                        action.act(val, None, i+1)
+
+            else: # 元素抽取模式
+                soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+                elements = soup.select(extractor.selector)
+                for i, element in enumerate(elements): # TODO
+                    val = extractor.extract(element, html)
+
+                    if callable(action): # 自定义函数
+                        action(val)
+                    else:
+                        action.act(val, element, i+1)
+
+
+# 文件下载任务
+class DownloadTask:
+    def __init__(self, url, element, n, savedir, filename): # TODO
+        self.url = url
+        self.element = element
+        self.n = n
+        self.savedir = savedir
+        self.filename = filename
+
+    def run(self):
+        # 用于格式化保存路径和文件名的上下文
+        context = {**self.element.attrs} if self.element else {}
+        context['basename'] = os.path.basename(self.url)
+        context['ext'] = os.path.splitext(self.url)[1]
+        context['n'] = self.n
+
+        savedir = self.savedir.format(**context)
+        filename = self.filename.format(**context)
+        _download(self.url, savedir, filename)
 
 
 # 元素的文本内容抽取器
@@ -41,19 +94,10 @@ class Download:
     def __init__(self, savedir, filename):
         self.savedir = savedir
         self.filename = filename
-
+        
     def act(self, val, element, n):
-        print('[INFO] Downloading {0}'.format(val))
-
-        # 用于格式化保存路径和文件名的上下文
-        context = {**element.attrs} if element else {}
-        context['basename'] = os.path.basename(val)
-        context['ext'] = os.path.splitext(val)[1]
-        context['n'] = n
-
-        savedir = self.savedir.format(**context)
-        filename = self.filename.format(**context)
-        _download(val, savedir, filename)
+        task = DownloadTask(val, element, n, self.savedir, self.filename)
+        q.put(task)
 
 
 def _download(url, savedir, filename):
@@ -67,13 +111,13 @@ class Enqueue:
     def __init__(self, level):
         self.level = level
     def act(self, val, element, n):
-        Q.put((self.level, val))
+        q.put(PageTask(self.level, val))
 
 
 # 注册初始URL
 def urls(func):
     for url in func():
-        Q.put((1, url))
+        q.put(PageTask(1, url))
     return func
 
 
@@ -85,38 +129,20 @@ def page(level):
     return decorator
 
 
-# 启动爬虫
-def start():
+def thread_func():
     while True:
-        level, url = Q.get()
-        print("[INFO] GET {0}".format(url))
-
-        html = requests.get(url).text
-        rules = RULE_DICT[level]()
-
-        for extractor, action in rules.items():
-
-            if isinstance(extractor, Re): # 正则抽取模式
-                vals = extractor.extract(None, html)
-                for i, val in enumerate(vals):
-                    if callable(action): # 自定义函数
-                        action(val)
-                    else:
-                        action.act(val, None, i+1)
-
-            else: # 元素抽取模式
-                soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
-                elements = soup.select(extractor.selector)
-                for i, element in enumerate(elements): # TODO
-                    val = extractor.extract(element, html)
-
-                    if callable(action): # 自定义函数
-                        action(val)
-                    else:
-                        action.act(val, element, i+1)
+        item = q.get()
+        print(item)
+        item.run()
+        q.task_done()
 
 
-        Q.task_done()
+# 启动爬虫
+def start(thead_num=3):
+    for i in range(thead_num):
+        t = threading.Thread(target=thread_func)
+        # t.daemon = True # TODO
+        t.start()
 
 
 if __name__ == "__main__":
