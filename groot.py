bb42146a43950e41badee3ebfc0abb2bede11f1b
manxisuo <manxisuo@gmail.com>
2019-08-08 23:34:29 +0800

    重构

--- groot.py
+++ groot.py
@@ -9,16 +9,23 @@ import os.path
 import threading
 
 session = requests.Session()
-session.headers['User-Agent'] = 'mozilla/5.0 (x11; linux x86_64) ' \
-    + 'applewebkit/537.36 (khtml, like gecko) ' \
-    + 'chrome/70.0.3538.102 safari/537.36'
+session.headers['User-Agent'] = \
+    'mozilla/5.0 (x11; linux x86_64) ' \
+    'applewebkit/537.36 (khtml, like gecko) ' \
+    'chrome/70.0.3538.102 safari/537.36'
 
 q = Queue() # URL队列
 RULE_DICT = {} # 用户注册的各级页面处理逻辑
 OLD_URLS = set()
 
+
+def identify(arg):
+    return arg
+
+
 def info(msg):
     print('[INFO] {0}'.format(msg))
+
 
 # 页面处理任务
 class PageTask:
@@ -32,32 +39,24 @@ class PageTask:
         rules = RULE_DICT[self.level]()
 
         for extractor, action in rules.items():
+            if callable(extractor):  # 自定义extractor函数
+                results = extractor(html)
+            else:
+                results = extractor.extract(html)
 
-            if isinstance(extractor, Re): # 正则抽取模式
-                vals = extractor.extract(None, html)
-                for i, val in enumerate(vals):
-                    if callable(action): # 自定义函数
-                        action(val)
-                    else:
-                        action.act(val, None, i+1)
-
-            else: # 元素抽取模式
-                soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
-                elements = soup.select(extractor.selector)
-                for i, element in enumerate(elements): # TODO
-                    val = extractor.extract(element, html)
-
-                    if callable(action): # 自定义函数
-                        action(val)
-                    else:
-                        action.act(val, element, i+1)
+            for i, result in enumerate(results):
+                if callable(action):  # 自定义action函数
+                    val, context = result
+                    action(val, context)
+                else:
+                    action.act(result, i + 1)
 
 
 # 文件下载任务
 class DownloadTask:
-    def __init__(self, url, element, n, savedir, filename): # TODO
+    def __init__(self, url, context, n, savedir, filename): # TODO
         self.url = url
-        self.element = element
+        self.context = context
         self.n = n
         self.savedir = savedir
         self.filename = filename
@@ -66,7 +65,7 @@ class DownloadTask:
         info('Download {0}'.format(urllib.parse.unquote(self.url)))
 
         # 用于格式化保存路径和文件名的上下文
-        context = {**self.element.attrs} if self.element else {}
+        context = {**self.context}
         context['basename'] = os.path.basename(self.url)
         context['ext'] = os.path.splitext(self.url)[1]
         context['n'] = self.n
@@ -78,39 +77,58 @@ class DownloadTask:
 
 # 元素的文本抽取器
 class Text:
-    def __init__(self, selector):
+    def __init__(self, selector, fn=identify):
         self.selector = selector
-    def extract(self, element, html):
-        return element.text
+        self.fn = fn
+
+    def extract(self, html):
+        soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+        elements = soup.select(self.selector)
+        for el in elements:
+            val = self.fn(el.text)
+            context = el.attrs
+            yield (val, context)
 
 
 # 元素的属性抽取器
 class Attr:
-    def __init__(self, selector, name):
+    def __init__(self, selector, name, fn=identify):
         self.selector = selector
         self.name = name
-    def extract(self, element, html):
-        return element.attrs[self.name]
+        self.fn = fn
+
+    def extract(self, html):
+        soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+        elements = soup.select(self.selector)
+        for el in elements:
+            val = self.fn(el.attrs[self.name])
+            context = el.attrs
+            yield (val, context)
 
 
 # 正则表达式抽取器
 class Re:
     def __init__(self, re_str):
         self.regexp = re.compile(re_str)
-    def extract(self, element, html):
-        return ((m.group(), *m.groups()) for m in self.regexp.finditer(html))
+
+    def extract(self, html):
+        for m in self.regexp.finditer(html):
+            val = m.group()
+            context = (val, *m.groups())
+            yield (val, context)
 
 
 # 动作：下载
 class Download:
-    def __init__(self, savedir, filename, fn=None):
+    def __init__(self, savedir, filename, fn=identify):
         self.savedir = savedir
         self.filename = filename
         self.fn = fn
-        
-    def act(self, val, element, n):
-        url = self.fn(val) if self.fn else val
-        task = DownloadTask(url, element, n, self.savedir, self.filename)
+
+    def act(self, result, n):
+        val, context = result
+        url = self.fn(val)
+        task = DownloadTask(url, context, n, self.savedir, self.filename)
         q.put(task)
 
 
@@ -120,14 +138,33 @@ def _download(url, savedir, filename):
     urllib.request.urlretrieve(url, os.path.join(savedir, filename))
 
 
-# 动作：URL入队列
-class Enqueue:
-    def __init__(self, level, fn=None):
-        self.level = level
-        self.fn = fn
+def format_context(format_str: str, context) -> str:
+    if not format_str or not context:
+        return format_str
 
-    def act(self, val, element, n):
-        url = self.fn(val) if self.fn else val
+    if type(context) is tuple:
+        return format_str.format(*context)
+    elif type(context) is dict:
+        return format_str.format(**context)
+    else:
+        return format_str
+
+
+# 动作：URL入队列
+# :custom 函数或字符串(支持{0}, {1}等格式)
+class Enqueue:
+    def __init__(self, level, custom=None):
+        self.level = level
+        self.custom = custom
+
+    def act(self, result, n):
+        val, context = result
+        if callable(self.custom):
+            url = self.custom(val, context)
+        elif type(self.custom) is str:
+            url = format_context(self.custom, context)
+        else:
+            url = val
         q.put(PageTask(self.level, url))
 
 
@@ -156,8 +193,8 @@ def thread_func():
 
 
 # 启动爬虫
-def start(thead_num=3):
-    for i in range(thead_num):
+def start(thread_num=3):
+    for i in range(thread_num):
         t = threading.Thread(target=thread_func)
         # t.daemon = True # TODO
         t.start()
@@ -165,3 +202,5 @@ def start(thead_num=3):
 
 if __name__ == "__main__":
     pass
+    t = Text('h1')
+    r = t.extract(None, '')
