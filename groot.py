e7d62a12f9eb6f88bce19abfefc57beb05ae1b1f
manxisuo <manxisuo@gmail.com>
2019-08-09 20:58:54 +0800

    update

--- groot.py
+++ groot.py
@@ -7,6 +7,7 @@ import re
 import os
 import os.path
 import threading
+from collections import namedtuple
 
 session = requests.Session()
 session.headers['User-Agent'] = \
@@ -27,6 +28,14 @@ def info(msg):
     print('[INFO] {0}'.format(msg))
 
 
+def _iterable(sth):
+    return hasattr(sth, '__iter__')
+
+
+# 从页面抽取的数据结果
+Result = namedtuple('Result', ['val', 'context'])
+
+
 # 页面处理任务
 class PageTask:
     def __init__(self, level, url):
@@ -43,27 +52,21 @@ class PageTask:
 
         for extractor, actions in rules.items():
             # 抽取结果
-            if callable(extractor):  # 自定义extractor函数
-                results = extractor(html)
-            else:
-                results = extractor.extract(html)
+            results = extractor.extract(html)
 
             # 执行动作(列表)
             for i, result in enumerate(results):
-                val, context = result
-                if not hasattr(actions, '__iter__'):  # 单个action时，可以不放到列表中
+                if not _iterable(actions):  # 单个action时，可以不放到列表中
                     actions = [actions]
                 for action in actions:
-                    action.act(val, context, i + 1)
+                    action.act(result, i + 1)
 
 
 # 文件下载任务
 class DownloadTask:
-    def __init__(self, url, context, n, savedir, filename): # TODO
+    def __init__(self, url, save_dir, filename):  # TODO
         self.url = url
-        self.context = context
-        self.n = n
-        self.savedir = savedir
+        self.save_dir = save_dir
         self.filename = filename
 
     def tid(self):
@@ -71,16 +74,7 @@ class DownloadTask:
 
     def run(self):
         info('Download {0}'.format(urllib.parse.unquote(self.url)))
-
-        # 用于格式化保存路径和文件名的上下文
-        context = {**self.context}
-        context['basename'] = os.path.basename(self.url)
-        context['ext'] = os.path.splitext(self.url)[1]
-        context['n'] = self.n
-
-        savedir = self.savedir.format(**context)
-        filename = self.filename.format(**context)
-        _download(self.url, savedir, filename)
+        _download(self.url, self.save_dir, self.filename)
 
 
 # 元素的文本抽取器
@@ -89,13 +83,13 @@ class Text:
         self.selector = selector
         self.fn = fn
 
-    def extract(self, html):
+    def extract(self, html) -> str:
         soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
         elements = soup.select(self.selector)
         for el in elements:
             val = self.fn(el.text)
             context = el.attrs
-            yield (val, context)
+            yield Result(val, context)
 
 
 # 元素的属性抽取器
@@ -105,13 +99,13 @@ class Attr:
         self.name = name
         self.fn = fn
 
-    def extract(self, html):
+    def extract(self, html) -> str:
         soup = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
         elements = soup.select(self.selector)
         for el in elements:
             val = self.fn(el.attrs[self.name])
             context = el.attrs
-            yield (val, context)
+            yield Result(val, context)
 
 
 # 正则表达式抽取器
@@ -119,44 +113,55 @@ class Re:
     def __init__(self, re_str):
         self.regexp = re.compile(re_str)
 
-    def extract(self, html):
+    def extract(self, html) -> str:
         for m in self.regexp.finditer(html):
             val = m.group()
             context = (val, *m.groups())
-            yield (val, context)
+            yield Result(val, context)
 
 
-# 动作：用户自定义
+# 用户自定义抽取器或动作
 class Func:
     def __init__(self, fn):
         self.fn = fn
 
-    def act(self, val, context, n):
-        self.fn(val, context)  # TODO 怎样更灵活地传参数
+    def act(self, result: Result, n):
+        self.fn(result)  # TODO 怎样更灵活地传参数
+
+    def extract(self, html) -> str:
+        return self.fn(html)
 
 
 # 动作：下载
 class Download:
-    def __init__(self, savedir, filename, fn=identify):
-        """
-        :param savedir: 文件保存目录
-        :param filename: 文件名
-        :param fn: (val, context) -> url 将提取的结果进行处理，得到下载URL
-        """
-        self.savedir = savedir
+    def __init__(self, save_dir, filename, custom=None):
+        self.save_dir = save_dir
         self.filename = filename
-        self.fn = fn
+        self.fn = _get_format_fn(custom) if type(custom) is str else custom
 
-    def act(self, val, context, n):
-        url = self.fn(val, context)  # TODO 函数的定位，以及参数
-        task = DownloadTask(url, context, n, self.savedir, self.filename)
+    def act(self, result: Result, n):
+        val, context = result
+        url = self.fn(result) if callable(self.fn) else result.val
+
+        # 用于格式化保存路径和文件名的上下文
+        context = {
+            **context,
+            'basename': os.path.basename(url),
+            'ext': os.path.splitext(url)[1],
+            'n': n
+        }
+
+        save_dir = self.save_dir.format(**context)
+        filename = self.filename.format(**context)
+
+        task = DownloadTask(url, save_dir, filename)
         q.put(task)
 
 
-def _download(url, savedir, filename):
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-    urllib.request.urlretrieve(url, os.path.join(savedir, filename))
+def _download(url, save_dir, filename):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    urllib.request.urlretrieve(url, os.path.join(save_dir, filename))
 
 
 def _format_context(format_str: str, context) -> str:
@@ -168,20 +173,21 @@ def _format_context(format_str: str, context) -> str:
     return format_str
 
 
+def _get_format_fn(format_str: str):
+    def fn(result: Result):
+        return _format_context(format_str, result.context)
+    return fn
+
+
 # 动作：URL入队列
 # :custom 函数或字符串(支持{0}, {1}等格式)
 class Enqueue:
     def __init__(self, level, custom=None):
         self.level = level
-        self.custom = custom
+        self.fn = _get_format_fn(custom) if type(custom) is str else custom
 
-    def act(self, val, context, n):
-        if callable(self.custom):
-            url = self.custom(val, context)  # TODO 函数的定位，以及参数
-        elif type(self.custom) is str:
-            url = _format_context(self.custom, context)
-        else:
-            url = val
+    def act(self, result: Result, n):
+        url = self.fn(result) if callable(self.fn) else result.val
         q.put(PageTask(self.level, url))
 
 
